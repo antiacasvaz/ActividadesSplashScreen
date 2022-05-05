b434add02cf4b8489df5f5bee854a88913c2723f
crisobradoiro <crisobradoiro@gmail.com>
2022-05-05 10:41:00 +0200

    Proxecto enteiro

--- app/src/main/java/com/cursokotlin/concelloteo/entroido.kt
+++ app/src/main/java/com/cursokotlin/concelloteo/entroido.kt
@@ -1,6 +1,7 @@
 package com.cursokotlin.concelloteo
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -26,6 +27,23 @@ class entroido : AppCompatActivity() {
         botonCacheiras.setOnClickListener {
             val acceder = Intent(this, mapaCacheiras::class.java)
             startActivity(acceder)
+        }
+
+        val facebook = findViewById<ImageButton>(R.id.facebook)
+        facebook.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://es-es.facebook.com/turismoteo/")))
+        }
+        val twitter = findViewById<ImageButton>(R.id.twitter)
+        twitter.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/turismoteo")))
+        }
+        val instagram = findViewById<ImageButton>(R.id.instagram)
+        instagram.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/accounts/login/?next=/turismoteo/")))
+        }
+        val info = findViewById<ImageButton>(R.id.info)
+        info.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://turismo.teo.gal/es/oficina-de-turismo")))
         }
     }
 }
